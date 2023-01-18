7ccaa4f474544af54e0cb33fae7e16e4cfd58a7f
Tode Temov <75773817+temov@users.noreply.github.com>
2023-01-18 02:36:38 +0100

    Add files via upload

--- Homework - Movies rent app/script.js
+++ Homework - Movies rent app/script.js
@@ -5,32 +5,29 @@ let btn = document.getElementById('button');
 let result = document.getElementById('result');
 
 
+let movies = ['Topgun', 'Gladiator', 'Titanic', 'Heat', 'Sing'];
 
 btn.addEventListener('click', function(){
-    let movies = ['Topgun', 'Gladiator', 'Titanic', 'Heat', 'Sing'];
+    let h1 = document.createElement('h1');
+    result.appendChild(h1);
 
     for(let i=0; i<movies.length;i++){
+        
+            if(inputMovie.value.toLowerCase() === movies[i].toLowerCase()){
 
-            if(inputMovie.value.toLowercase() === movies[i].toLowerCase()){
-
-                let h1 = document.createElement('h1');
                 h1.innerHTML = 'The movie can be rented';
                 h1.style.color = 'green';
-                result.appendChild(h1);
-
-            }else if (inputMovie.value.toLowercase() !== movies[i].toLowerCase()){
-
-                let h1 = document.createElement('h1');
+                return result.innerHTML;
+                
+            }else{
                 h1.innerHTML = 'The movie cant be rented';
                 h1.style.color = 'red';
-                result.appendChild(h1);
+                
             }
-       else if (inputMovie.value.toLowerCase() === ''){
-                let h1 = document.createElement('h1');
-                h1.innerHTML = 'Please enter movie Title';
-                result.appendChild(h1);
-        }
+         
     }
     
-
+    inputMovie.value = '';
+    
+        
 })
