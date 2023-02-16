7d0da2b71c20ee3dbbfb86d0c91aaba61755ff99
Tode Temov <75773817+temov@users.noreply.github.com>
2023-02-16 02:37:15 +0100

    Add files via upload

--- Homeworks - Creating object templates/script.js
+++ Homeworks - Creating object templates/script.js
@@ -32,161 +32,47 @@
 // * StartSubject - accepts Subject object and adds it to the CurrentSubject property but only if the student has an Academy object in the Academy property and that subject exists in the academy. If not, give error in console and do not set the CurrentSubject property
 
 
-
-
-
-// function Academy(academyName, studentsArray, subjectsArray, dateStart, dateEnd, numberOfSubjects) {
-
-//     this.name = academyName
-//     this.students = studentsArray
-//     this.subjects = subjectsArray
-//     this.start = dateStart
-//     this.end = dateEnd
-//     this.numberOfClasses = numberOfSubjects*10 //  function sum(subjectArr){ for()..number of subjects}
-//     this.printStd = function(studentsArray) {
-
-//         for(let student of studentsArray){
-
-//             console.log(student);
-//                 }
-    
-//          }
-//     this.printSub = function(subjectsArray) {
-
-//             for(let subject of subjectsArray){
-
-//                 console.log(subject);
-//                }
-        
-//         }
-   
-//     }
-
-//     let academyOne = Object.create(new Academy('Web Design', ['Tode Temov', 'Pero Nakov'], ['HTML', 'CSS'], '22-10-2022','22-10-2023',7 ))
-//     console.log(academyOne)
-//     academyOne.printStd(['Tode Temov', 'Pero Nakov'])
-//     academyOne.printSub(['HTML', 'CSS'])
-
-// function Subject(subjectTitle, isElective, academyObj, studentsArray) {
-
-//         this.subjectTitle = subjectTitle
-//         this.numberOfClasses = 10
-//         this.isElective = isElective
-//         this.academy = academyObj
-//         this.students = studentsArray
-        
-//         this.overrideClasses = function(number) {
-
-//            if( number > 3){
-
-//             this.numberOfClasses = number;
-//             console.log(`Number of classes is ${this.numberOfClasses}`)
-//            }else{
-
-//             console.log("Please enter bigger number than 3")
-//            }
-        
-//              }
-       
-       
-//         }
-
-//         let subject1 = Object.create(new Subject('Angular', true, academyOne, ['Kiro Fetak', 'Pero Nakov', 'Mirka Ginova']))
-
-//         console.log(subject1)
-
-//         subject1.overrideClasses(4)
-
-//         console.log(`Number of classes is ${subject1.numberOfClasses}`)
-
-// function Student(firstName,lastName,age,academyObj) {
-
-//             this.firstName = firstName
-//             this.lastName = lastName
-//             this.age = age
-//             this.completedSubjects = []
-//             this.academy = null
-//             this.currentSubject = null
-//             this.startAcademy = function(academyObj) {
-    
-//                         this.academy = academyObj;
-    
-            
-//                  }
-//             this.startSubject = function(subjectObj) {
-
-//                             for (i=0; i<academyObj.subjects.length; i++){
-
-//                             if( (this.academy === academyObj) && (this.currentSubject === academyObj.subjects[i])){
-
-//                                 this.currentSubject = subjectObj;
-//                             }else{
-                                
-//                                 console.log(`Error`);
-//                                 this.currentSubject = null
-//                             }
-
-//                         }
-                 
-//                       }
-           
-           
-//             }
-
-//             let student1 = Object.create(new Student('Georgi','Mitkov', 32, academyOne))
-//             console.log(student1);
-//             student1.startAcademy(academyOne)
-//             console.log(`${student1.academy}`)
-
-//             student1.startSubject(subject1)
-//             console.log(student1.currentSubject)
-
-            
  // ## Exercise 2
 // Make the functions StartAcademy and StartSubject dynamic.
 // * StartAcademy - When the student calls StartAcademy, the student should also be added to the Academy property Students ( The academy that he is starting )
 // * StartSubject - When the student calls StartSubject the student should also be added to the Subject property Students ( The subject that he is starting ). If there was another subject in the CurrentSubject property, that subject should be transferred to CompletedSubjects and then add the new Subject
 
-function Academy(academyName, studentsArray, subjectsArray, dateStart, dateEnd, numberOfSubjects) {
+
+function Academy(academyName, dateStart, dateEnd, numberOfSubjects) {
 
     this.name = academyName
-    this.students = studentsArray
-    this.subjects = subjectsArray
+    this.students = []
+    this.subjects = []
     this.start = dateStart
     this.end = dateEnd
-    this.numberOfClasses = numberOfSubjects*10 //  function sum(subjectArr){ for()..number of subjects}
-    this.printStd = function(studentsArray) {
+    this.numberOfClasses = numberOfSubjects*10 
+    this.printStd = function() {
 
-        for(let student of studentsArray){
-
-            console.log(student);
+                for( let i =0; i< this.students.length;i++){
+            console.log(this.students[i]);
                 }
-
-         }
-    this.printSub = function(subjectsArray) {
-
-            for(let subject of subjectsArray){
-
-                console.log(subject);
-               }
+         
     
-        }
+         }
+    this.printSub = function() {
 
+        for( let i =0; i< this.subjects.length;i++){
+            console.log(this.subjects[i]);
+                }
+   
     }
+}
 
-    let academyOne = Object.create(new Academy('Web Design', ['Tode Temov', 'Pero Nakov'], ['HTML', 'CSS'], '22-10-2022','22-10-2023',7 ))
-    console.log(academyOne)
-    academyOne.printStd(['Tode Temov', 'Pero Nakov'])
-    academyOne.printSub(['HTML', 'CSS'])
+   
 
-function Subject(subjectTitle, isElective, academyObj, studentsArray) {
+function Subject(subjectTitle, isElective,academyObj) {
 
         this.subjectTitle = subjectTitle
         this.numberOfClasses = 10
         this.isElective = isElective
         this.academy = academyObj
-        this.students = studentsArray
-    
+        this.students = []
+        
         this.overrideClasses = function(number) {
 
            if( number > 3){
@@ -197,21 +83,15 @@ function Subject(subjectTitle, isElective, academyObj, studentsArray) {
 
             console.log("Please enter bigger number than 3")
            }
-    
+        
              }
-   
-   
+       
+       
         }
 
-        let subject1 = Object.create(new Subject('Angular', true, academyOne, ['Kiro Fetak', 'Pero Nakov', 'Mirka Ginova']))
+      
 
-        console.log(subject1)
-
-        subject1.overrideClasses(4)
-
-        console.log(`Number of classes is ${subject1.numberOfClasses}`)
-
-function Student(firstName,lastName,age,completedSubjects,academyObj,currentSubject) {
+function Student(firstName,lastName,age) {
 
             this.firstName = firstName
             this.lastName = lastName
@@ -220,39 +100,94 @@ function Student(firstName,lastName,age,completedSubjects,academyObj,currentSubj
             this.academy = null
             this.currentSubject = null
             this.startAcademy = function(academyObj) {
-
+    
                         this.academy = academyObj;
-                        academyObj.students.push(`${this.firstName} ${this.lastName}`);
-        
+                        academyObj.students.push(this);
+    
+            
                  }
             this.startSubject = function(subjectObj) {
 
-    
-                                subjectObj.students.push(`${this.firstName} ${this.lastName}`);
+                                
 
-                            if((this.currentSubject !== null) &&(this.currentSubject !== '')){
+                            if( (this.academy !== null) && (this.academy.subjects.includes(subjectObj))){
 
-                                let current = this.currentSubject;
+                                if(this.currentSubject !== null){
 
-                                completedSubjects.push(current);
+                                   
+                                    this.completedSubjects.push(this.currentSubject)
+                                }
 
-
-                                this.currentSubject = subjectObj.subjectTitle;
+                                this.currentSubject = subjectObj;
+                                subjectObj.students.push(this);
+                            }else{
+                                
+                                console.log(`Error`);
+                                
                             }
-             
-                      }
-       
-       
-            }
-            let student1 = Object.create(new Student('Georgi','Mitkov', 32, academyOne))
-            let student2 = Object.create(new Student('Panko','Brasnarov', 28, academyOne))
-            console.log(student1);
+                        }
 
-            student1.startAcademy(academyOne)
-            student2.startAcademy(academyOne)
-            console.log(`${student1.academy}`)
-            console.log(academyOne.students)
+        }
+                 
+                      
+           
+           
+                      let academyOne = Object.create(new Academy('Web Design','22-10-2022','22-10-2023',7 ))
+                      let academyTwo = Object.create(new Academy('Graphic Design','22-05-2022','22-11-2022',5 ))
+                      let academyThree = Object.create(new Academy('DevOps','22-11-2022','22-06-2023',6 ))
 
-            student1.startSubject(subject1)
-            console.log(student1.completedSubjects);
-            console.log(student1.currentSubject);
+                     
+
+            let subject1 = Object.create(new Subject('Angular', true,academyOne))
+            let subject2 = Object.create(new Subject('HTML', false,academyTwo))
+            let subject3 = Object.create(new Subject('CSS', false,academyThree))
+
+            console.log(subject1.academy)
+
+            subject1.overrideClasses(4)
+            console.log(`Number of classes is ${subject1.numberOfClasses}`)
+
+            let student1 = Object.create(new Student('Georgi','Mitkov', 32))
+            let student2 = Object.create(new Student('Petko','Petkov', 27))
+            let student3 = Object.create(new Student('Kiro','Popov', 22))
+            academyOne.students.push(student1,student2,student3)
+            academyOne.subjects.push(subject1,subject2,subject3)
+
+            subject1.students.push(student1)
+
+           
+                      student1.startSubject(subject1)
+
+                      console.log(student1.completedSubjects)
+                      console.log(student1.currentSubject)
+          
+
+            // student1.startSubject(subject1)
+            // console.log(student1.currentSubject)
+
+            let student4 = Object.create(new Student('Mirko','Petrov', 25))
+
+            student4.startAcademy(academyOne)
+            student4.startSubject(subject2)
+            student4.startSubject(subject3)
+           
+            console.log(student4)
+
+            academyOne.printStd()
+        
+
+            console.log(academyOne)
+
+           
+        
+
+            student3.startAcademy(academyTwo)
+            console.log(academyTwo.students)
+            
+
+            
+           
+            
+            
+
+
